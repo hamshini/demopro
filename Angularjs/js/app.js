ecb0bdb6d0f8c219a611e9b31615e9388263a550
hamshini <hamshinikkdi88@gmail.com>
2018-01-19 16:10:30 +0530

    AJS 19/01/2018

--- Angularjs/js/app.js
+++ Angularjs/js/app.js
@@ -1,25 +1,18 @@
-(function(){
-    var app=angular.module("demo",[]);
-    app.controller("StoreController",function(){
-    this.product=item
+(function () {
+    var app = angular.module("demo", []);
+    app.controller("StoreController", function ($http) {
+        var pb = this;
+        this.contacts = [];
+        this.delete = function(loc) {
+            pb.contacts.splice(loc, 1)
+        };
+        this.toggleEdit = function(c) {
+            c.editable = !c.editable?true:false;
+        };
+        $http.get("phone-book.json").then(function(res) {
+            pb.contacts = res.data.contacts
+        },function(res) {
+            console.log("Error")
+        })
     });
-    var item=[{
-        name:"paste",
-        price:18.90,
-        canPurchase:true,
-        SoldOut:false,
-        description:"lorem is dummy."
-    },{
-        name:"soap",
-        price:18.90,
-        canPurchase:true,
-        SoldOut:false,
-        description:"lorem is dummy."
-    }, 
-    {name:"shampooo",
-    price:18.90,
-    canPurchase:true,
-    SoldOut:false,
-    description:"lorem is dummy."
-},];
 })();
