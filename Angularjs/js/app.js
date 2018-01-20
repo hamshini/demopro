1ceb9a473ed6fe0f8f072b62dd02cb6e7e939202
hamshinir <hamshinikkdi88@gmail.com>
2018-01-20 13:54:41 +0530

    AJS 20-jan-2018

--- Angularjs/js/app.js
+++ Angularjs/js/app.js
@@ -1,18 +1,63 @@
 (function () {
-    var app = angular.module("demo", []);
-    app.controller("StoreController", function ($http) {
-        var pb = this;
-        this.contacts = [];
-        this.delete = function(loc) {
-            pb.contacts.splice(loc, 1)
-        };
-        this.toggleEdit = function(c) {
-            c.editable = !c.editable?true:false;
-        };
-        $http.get("phone-book.json").then(function(res) {
-            pb.contacts = res.data.contacts
-        },function(res) {
-            console.log("Error")
-        })
+    var app = angular.module("demo", ["ngRoute"]);
+    app.config(function ($routeProvider, $locationProvider) {
+        $locationProvider.hashPrefix("");
+        $routeProvider.when("/new", {
+            templateUrl: "views/add-contact.html",
+            controller: function ($http) {
+                this.contacts = {};
+                this.show = false;
+                var pb = this;
+                this.addContact = function (contact) {
+                    $http.post("http://172.16.72.216:3000/contacts", contact).then(function (res) {
+                        console.log("updated");
+                        pb.show = true;
+                    }, function (res) {});
+                }
+            },
+            controllerAs: "store"
+        }).when("/home", {
+            templateUrl: "views/view-contacts.html",
+            controller: function ($http) {
+                this.contacts = [];
+                var pb = this;
+                $http.get("http://172.16.72.216:3000/contacts").then(function (res) {
+                    pb.contacts = res.data
+                }, function (res) {});
+            },
+            controllerAs: "store"
+        }).when("/edit/:id", {
+            templateUrl: "views/edit-contact.html",
+            controller: function ($http, $routeParams) {
+                var pb = this;
+                this.show = false;
+                this.contact = {};
+                this.updateContact = function (contact) {
+                    $http.put("http://172.16.72.216:3000/contacts/" + contactId, contact).then(function (res) {
+                        console.log("updated");
+                        pb.show = true;
+                    }, function (res) {});
+                }
+                var contactId = $routeParams["id"];
+                $http.get("http://172.16.72.216:3000/contacts/" + contactId).then(function (res) {
+                    pb.contact = res.data
+                }, function (res) {});
+            },
+            controllerAs: "store"
+        }).when("/delete/:id", {
+            templateUrl: "views/delete-contact.html",
+            controller: function ($http, $routeParams) {
+                var pb = this;
+                this.show = false;
+                var contactId = $routeParams["id"];
+                this.id = contactId;
+                $http.delete("http://172.16.72.216:3000/contacts/" + contactId).then(function (res) {
+                    pb.show = true;
+                }, function (res) {});
+            },
+            controllerAs: "store"
+        }).otherwise({
+            redirectTo: "/home"
+        });
     });
 })();
